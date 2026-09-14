9623abf7098364349756517d6ee95311658d4b0b
ebaysh3-prog <ebaysh3@gmail.com>
2026-09-14 22:33:57 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,11 +9,6 @@ try {
 
   const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-  console.log('UV keys:', Object.keys(uvMod));
-  console.log('BMX keys:', Object.keys(bmxMod));
-  console.log('wisp.server keys:', Object.keys(wispMod.server || {}));
-  console.log('wisp.server.default keys:', wispMod.server?.default ? Object.keys(wispMod.server.default) : 'none');
-
   const app = express();
   const server = createServer(app);
 
@@ -21,22 +16,8 @@ try {
   app.use('/bmx/', express.static(bmxMod.baremuxPath));
   app.use(express.static(path.join(__dirname, '..', 'static')));
 
-  const WispClass = wispMod.server?.WispServer
-    || wispMod.server?.default?.WispServer
-    || wispMod.server?.Server
-    || wispMod.server?.default?.Server
-    || wispMod.server?.default;
-
-  if (typeof WispClass !== 'function') {
-    console.error('WispClass is not a function:', typeof WispClass, WispClass);
-    process.exit(1);
-  }
-
-  const wisp = new WispClass({ allowed_origins: [/.*/] });
-
   server.on('upgrade', (req, socket, head) => {
-    const route = wisp.routeUpgrade || wisp.routeRequest || wisp.handleUpgrade;
-    route.call(wisp, req, socket, head);
+    wispMod.server.routeRequest(req, socket, head);
   });
 
   const PORT = process.env.PORT || 3000;
