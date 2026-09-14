a59730e6be751c3efb82f23a29b45b6acad90355
ebaysh3-prog <ebaysh3@gmail.com>
2026-09-14 22:19:55 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import express from 'express';
 import { createServer } from 'node:http';
 import { uvPath } from '@titaniumnetwork-dev/ultraviolet';
 import { baremuxPath } from '@mercuryworkshop/bare-mux/node';
-import wispPkg from 'wisp-server-node';
-const { createWisp } = wispPkg;
+import { wisp } from '@mercuryworkshop/wisp-js';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
@@ -16,7 +15,9 @@ app.use('/uv/', express.static(uvPath));
 app.use('/bmx/', express.static(baremuxPath));
 app.use(express.static(path.join(__dirname, '..', 'static')));
 
-const wisp = createWisp(server);
+server.on('upgrade', (req, socket, head) => {
+  wisp.routeRequest(req, socket, head);
+});
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`StudyHub live on :${PORT}`));
